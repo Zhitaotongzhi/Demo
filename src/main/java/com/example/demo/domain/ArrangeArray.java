d6a32f048a7a1fed870ce6afd00683393f780ff8
sunzy <1930747581@qq.com>
2023-06-16 22:14:39 +0800

    6-16 sun

---
+++ src/main/java/com/example/demo/domain/ArrangeArray.java
@@ -0,0 +1,21 @@
+package com.example.demo.domain;
+
+public class ArrangeArray {
+    private String[] dates;
+    private ArrangeElement[] workers;
+    public String[] getDates() {
+        return dates;
+    }
+
+    public void setDate(String[] dates) {
+        this.dates = dates;
+    }
+
+    public ArrangeElement[] getWorkers() {
+        return workers;
+    }
+
+    public void setWorks(ArrangeElement[] workers) {
+        this.workers = workers;
+    }
+}
