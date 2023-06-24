b50a1a8eb1e955df5f17243c8ae1de0236cc2f10
sunzy <1930747581@qq.com>
2023-06-24 14:41:55 +0800

    6-24 sun -2

--- src/main/java/com/example/demo/controller/AssistController.java
+++ src/main/java/com/example/demo/controller/AssistController.java
@@ -2,10 +2,13 @@ package com.example.demo.controller;
 
 import com.example.demo.dao.AssistDao;
 import com.example.demo.domain.Assist;
+import com.example.demo.domain.Record;
 import com.example.demo.utils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,7 +30,7 @@ public class AssistController {
         }
     }
 
-    //管理员查看咨询记录
+    //管理员查看求助记录
     @GetMapping("/list")
     public Result list(@RequestParam String pageNum, @RequestParam String pageSize, @RequestParam String counselorName, @RequestParam String startDate, @RequestParam String endDate){
         int i = Integer.parseInt(pageNum);
@@ -65,4 +68,106 @@ public class AssistController {
             return Result.failure("2", "操作失败");
         }
     }
+
+    //督导查看其下咨询师的求助记录
+    @GetMapping("/bindList/{supervisorUsername}")
+    public Result bindAssistList(@RequestParam String pageNum, @RequestParam String pageSize, @RequestParam String counselorName, @PathVariable String supervisorUsername, @RequestParam String startDate, @RequestParam String endDate){
+        int i = Integer.parseInt(pageNum);
+        int j = Integer.parseInt(pageSize);
+        List<String> list = assistDao.findAllBindByUsername(supervisorUsername);
+        if(counselorName.isEmpty() && startDate.isEmpty()){
+            List<Assist> assists = assistDao.findAllBindAssist((i - 1) * j, j, supervisorUsername);
+            long total = assists.size();
+            Map<String, Object> maps = new HashMap<>();
+            maps.put("total", total);
+            maps.put("assists", assists);
+            return Result.success(maps);
+        }else if(!counselorName.isEmpty()  && startDate.isEmpty()){
+            List<Assist> assists = assistDao.findAllBindAssistByConName((i - 1) * j, j, counselorName, list);
+            long total = assistDao.totalByConName(counselorName);
+            Map<String, Object> maps = new HashMap<>();
+            maps.put("total", total);
+            maps.put("assists", assists);
+            return Result.success(maps);
+        }
+        else if(counselorName.isEmpty()  && !startDate.isEmpty()){
+            List<Assist> assists = assistDao.findAllBindAssistByTime((i - 1) * j, j, startDate, endDate, list);
+            long total = assistDao.totalByTime(startDate, endDate);
+            Map<String, Object> maps = new HashMap<>();
+            maps.put("total", total);
+            maps.put("assists", assists);
+            return Result.success(maps);
+        }else if(!counselorName.isEmpty() && !startDate.isEmpty()){
+            List<Assist> assists = assistDao.findAllBindAssistByAll((i - 1) * j, j, counselorName, startDate, endDate);
+            long total = assistDao.totalByAll(counselorName, startDate, endDate);
+            Map<String, Object> maps = new HashMap<>();
+            maps.put("total", total);
+            maps.put("assists", assists);
+            return Result.success(maps);
+        }else{
+            return Result.failure("2", "操作失败");
+        }
+    }
+
+//    //统计今天总的求助时长
+//    @GetMapping("/totalTime")
+//    public Result totalTime(){
+//        LocalDate date = LocalDate.now();
+//        Date totalTime = assistDao.totalTimeCount(date.toString());
+//        Map<String, Object> total = new HashMap<>();
+//        total.put("totalTime", totalTime);
+//        return Result.success(total);
+//    }
+
+//    //督导今日响应求助总时长
+//    @GetMapping("/supervisorTotalTime/{supervisorUsername}")
+//    public Result conTotalTime(@PathVariable String supervisorUsername){
+//        LocalDate date = LocalDate.now();
+//        System.out.println(date);
+//        String name = supervisorDao.findByUsername(supervisorUsername);
+//        Date totalTime = assistDao.supervisorTotalTime(supervisorUsername, date.toString());
+//        Map<String, Object> total = new HashMap<>();
+//        total.put("totalTime", totalTime);
+//        return Result.success(total);
+//    }
+
+//    //今日统计求助总数
+//    @GetMapping("/totalCount")
+//    public Result totalCount(){
+//        LocalDate date = LocalDate.now();
+//        int total = assistDao.totalCount(date.toString());
+//        Map<String, Object> totals = new HashMap<>();
+//        totals.put("total", total);
+//        return Result.success(totals);
+//    }
+
+
+//    //督导今日响应求助总数
+//    @GetMapping("/supervisorTotalCount/{username}")
+//    public Result conTotalCount(@PathVariable String username){
+//        LocalDate date = LocalDate.now();
+//        String counselorName = consultantDao.findName(username);
+//        int total = assistDao.counselorRecordCount(counselorName, date.toString());
+//        Map<String, Object> totals = new HashMap<>();
+//        totals.put("total", total);
+//        return Result.success(totals);
+//    }
+//
+//    //显示最近的求助记录
+//    @GetMapping("/currentRecords")
+//    public Result currentRecord(){
+//        List<Record> records = recordDao.currentRecord(5);
+//        Map<String, Object> record = new HashMap<>();
+//        record.put("records", records);
+//        return Result.success(record);
+//    }
+//
+//    //正在进行的求助总数
+//    @GetMapping("/currentCount")
+//    public Result currentCount(){
+//        long total = recordDao.currentCount();
+//        Map<String, Object> count = new HashMap<>();
+//        count.put("total", total);
+//        return Result.success(count);
+//    }
 }
